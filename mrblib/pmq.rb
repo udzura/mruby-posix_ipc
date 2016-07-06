326744ddcf6df3b420e86137b8548f4a9b87312f
Uchio KONDO <udzura@pepabo.com>
2016-07-06 17:08:22 +0900

    Fix API, and respect queue size and message size

--- mrblib/pmq.rb
+++ mrblib/pmq.rb
@@ -1,5 +1,31 @@
 class PMQ
+  def self.create(name, options={})
+    queue_size = options[:queue_size] || 1
+    message_size = options[:message_size] || -1
+    if mode = options[:mode]
+      if mode.is_a? String
+        _mode = PMQ::O_CREAT
+        (0...mode.length).each do |idx|
+          case mode[idx]
+          when "r"
+            _mode |= PMQ::O_RDONLY
+          when "w"
+            _mode |= PMQ::O_WRONLY
+          else
+            raise "Invalid mode letter: #{mode[idx]}"
+          end
+        end
+        mode = _mode
+      end
+    end
+    mode ||= PMQ::O_CREAT|PMQ::O_RDWR
+
+    new(name, mode, queue_size, message_size)
+  end
+
+  alias size maxmsg
+
   def inspect
-    "#<PMQ name=#{self.name}>"
+    "#<PMQ name=#{self.name} size=#{self.size}>"
   end
 end
--- src/mrb_posix_ipc.c
+++ src/mrb_posix_ipc.c
@@ -6,6 +6,7 @@
 ** See Copyright Notice in LICENSE
 */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <fcntl.h>
@@ -38,11 +39,30 @@ static const struct mrb_data_type mrb_pmq_data_type = {
   "mrb_pmq_data", mrb_pmq_free,
 };
 
+static int get_system_msgsize_max(mrb_state *mrb)
+{
+  const char *msgsize_max_path = "/proc/sys/fs/mqueue/msgsize_max";
+  size_t maxlen = 9; /* HARD_MSGSIZEMAX=16777216 */
+  char buf[maxlen];
+  FILE *f = fopen(msgsize_max_path, "r");
+  if(f == NULL)
+    return -1;
+
+  size_t len = fread(buf, maxlen, 1, f);
+  if(len < 0)
+    return -1;
+
+  mrb_value str = mrb_str_new_cstr(mrb, buf);
+  str = mrb_funcall(mrb, str, "chomp", 0);
+  mrb_value size = mrb_str_to_inum(mrb, str, 10, FALSE);
+  return mrb_fixnum(size);
+}
+
 static mrb_value mrb_pmq_init(mrb_state *mrb, mrb_value self)
 {
   mrb_pmq_data *data;
   char *name;
-  mrb_int flag;
+  mrb_int flag, queuesize, msgsize;
   struct mq_attr *attr;
 
   data = (mrb_pmq_data *)DATA_PTR(self);
@@ -52,17 +72,25 @@ static mrb_value mrb_pmq_init(mrb_state *mrb, mrb_value self)
   DATA_TYPE(self) = &mrb_pmq_data_type;
   DATA_PTR(self) = NULL;
 
-  mrb_get_args(mrb, "zi", &name, &flag);
+  mrb_get_args(mrb, "ziii", &name, &flag, &queuesize, &msgsize);
   data = (mrb_pmq_data *)mrb_malloc(mrb, sizeof(mrb_pmq_data));
   attr = (struct mq_attr *)malloc(sizeof(struct mq_attr));
-  /* TODO: to be configurable */
   attr->mq_flags = 0;
-  attr->mq_maxmsg = 1;
-  attr->mq_msgsize = 100;
+  attr->mq_maxmsg = queuesize;
+  if(msgsize < 0) {
+    int maxsize = get_system_msgsize_max(mrb);
+    if(maxsize < 0) {
+      mrb_sys_fail(mrb, "cannot open msgsize_max.");
+    }
+    attr->mq_msgsize = maxsize;
+  } else {
+    attr->mq_msgsize = msgsize;
+  }
   attr->mq_curmsgs = 0;
 
   data->mqd = mq_open(name, (int)flag, 0644, attr);
   if(data->mqd < 0) {
+    perror("debug");
     mrb_sys_fail(mrb, "mq_open failed.");
   }
   data->attr = attr;
@@ -130,6 +158,11 @@ static mrb_value mrb_pmq_name(mrb_state *mrb, mrb_value self)
   return mrb_str_new_cstr(mrb, ((mrb_pmq_data *) DATA_PTR(self))->name);
 }
 
+static mrb_value mrb_pmq_maxmsg(mrb_state *mrb, mrb_value self)
+{
+  return mrb_fixnum_value(((mrb_pmq_data *) DATA_PTR(self))->attr->mq_maxmsg);
+}
+
 static mrb_value mrb_pmq_is_unlinked(mrb_state *mrb, mrb_value self)
 {
   return mrb_bool_value(((mrb_pmq_data *) DATA_PTR(self))->unlinked);
@@ -139,12 +172,13 @@ void mrb_mruby_posix_ipc_gem_init(mrb_state *mrb)
 {
   struct RClass *pmq;
   pmq = mrb_define_class(mrb, "PMQ", mrb->object_class);
-  mrb_define_method(mrb, pmq, "initialize", mrb_pmq_init,        MRB_ARGS_REQ(2));
+  mrb_define_method(mrb, pmq, "initialize", mrb_pmq_init,        MRB_ARGS_REQ(4));
   mrb_define_method(mrb, pmq, "send",       mrb_pmq_send,        MRB_ARGS_ARG(1,1));
   mrb_define_method(mrb, pmq, "receive",    mrb_pmq_receive,     MRB_ARGS_NONE());
   mrb_define_method(mrb, pmq, "close",      mrb_pmq_close,       MRB_ARGS_NONE());
   mrb_define_method(mrb, pmq, "unlink",     mrb_pmq_unlink,      MRB_ARGS_NONE());
   mrb_define_method(mrb, pmq, "name",       mrb_pmq_name,        MRB_ARGS_NONE());
+  mrb_define_method(mrb, pmq, "maxmsg",     mrb_pmq_maxmsg,      MRB_ARGS_NONE());
   mrb_define_method(mrb, pmq, "unlinked?",  mrb_pmq_is_unlinked, MRB_ARGS_NONE());
 
   /* Flags for open file mode */
