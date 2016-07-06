e8778b7b07c668bd8882f311c08f33c84d95b275
Uchio KONDO <udzura@pepabo.com>
2016-07-06 19:01:40 +0900

    Add new test

--- test/posixsem.rb
+++ test/posixsem.rb
@@ -1,6 +1,19 @@
 assert("PSem.new") do
   psem = PSem.open "/hello"
   assert_true psem.is_a?(PSem)
+
+  psem.unlink  rescue nil
+end
+
+assert("PSem.new default value") do
+  psem = PSem.open "/hello_1", value: 3
+  assert_equal psem.value, 3
+
+  3.times do
+    psem.wait
+  end
+  assert_equal psem.trywait, -1
+
   psem.unlink rescue nil
 end
 
