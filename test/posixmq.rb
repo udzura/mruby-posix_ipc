e7553807dacd2d561891ca57c0c5df4ed4a96866
Uchio KONDO <udzura@pepabo.com>
2016-07-22 11:33:17 +0900

    Add test case

--- test/posixmq.rb
+++ test/posixmq.rb
@@ -17,3 +17,23 @@ assert("PMQ send and receive") do
 
   r.unlink rescue nil
 end
+
+assert("PMQ send and receive #2") do
+  r, w = PMQ.channel2 "/hello_3", size: 10
+  p = Process.fork do
+    sleep 0.5
+    w.send "foo"
+    w.send "bar"
+    w.send "buz"
+  end
+
+  res1 = r.receive
+  res2 = r.receive
+  res3 = r.receive
+  Process.waitpid p
+  assert_equal "foo", res1
+  assert_equal "bar", res2
+  assert_equal "buz", res3
+
+  r.unlink rescue nil
+end
